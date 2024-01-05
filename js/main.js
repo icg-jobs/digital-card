6762ad484adfa29a278a589ae26295f1b018bc4c
田川　貢 <mitsugu@tagawamitsuginoMacBook-Pro.local>
2024-01-05 14:59:24 +0900

    test

---
+++ js/main.js
@@ -0,0 +1,152 @@
+// ページ内リンク
+// $('a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+// 	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+// 	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+// 	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+// 	return false;
+// });
+
+//上*****************************************************
+var unit = 100,
+    canvasList, // キャンバスの配列
+    info = {}, // 全キャンバス共通の描画情報
+    colorList; // 各キャンバスの色情報
+
+/**
+ * Init function.
+ * 
+ * Initialize variables and begin the animation.
+ */
+function init() {
+    info.seconds = 0;
+    info.t = 0;
+		canvasList = [];
+    colorList = [];
+    // canvas1個めの色指定
+    canvasList.push(document.getElementById("waveCanvas"));
+    colorList.push(['#DCECFF']);
+	// 各キャンバスの初期化
+		for(var canvasIndex in canvasList) {
+        var canvas = canvasList[canvasIndex];
+        canvas.width = document.documentElement.clientWidth; //Canvasのwidthをウィンドウの幅に合わせる
+        canvas.height = 300;//波の高さ
+        canvas.contextCache = canvas.getContext("2d");
+    }
+    // 共通の更新処理呼び出し
+		update();
+}
+
+function update() {
+		for(var canvasIndex in canvasList) {
+        var canvas = canvasList[canvasIndex];
+        // 各キャンバスの描画
+        draw(canvas, colorList[canvasIndex]);
+    }
+    // 共通の描画情報の更新
+    info.seconds = info.seconds + .014;
+    info.t = info.seconds*Math.PI;
+    // 自身の再起呼び出し
+    setTimeout(update, 35);
+}
+
+/**
+ * Draw animation function.
+ * 
+ * This function draws one frame of the animation, waits 20ms, and then calls
+ * itself again.
+ */
+function draw(canvas, color) {
+		// 対象のcanvasのコンテキストを取得
+    var context = canvas.contextCache;
+    // キャンバスの描画をクリア
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    //波を描画 drawWave(canvas, color[数字（波の数を0から数えて指定）], 透過, 波の幅のzoom,波の開始位置の遅れ )
+    drawWave(canvas, color[0], 1, 3, 0);//drawWave(canvas, color[0],0.5, 3, 0);とすると透過50%の波が出来る
+}
+
+/**
+* 波を描画
+* drawWave(色, 不透明度, 波の幅のzoom, 波の開始位置の遅れ)
+*/
+function drawWave(canvas, color, alpha, zoom, delay) {
+		var context = canvas.contextCache;
+    context.fillStyle = color;//塗りの色
+    context.globalAlpha = alpha;
+    context.beginPath(); //パスの開始
+    drawSine(canvas, info.t / 0.5, zoom, delay);
+    context.lineTo(canvas.width + 10, canvas.height); //パスをCanvasの右下へ
+    context.lineTo(0, canvas.height); //パスをCanvasの左下へ
+    context.closePath() //パスを閉じる
+    context.fill(); //波を塗りつぶす
+}
+
+/**
+ * Function to draw sine
+ * 
+ * The sine curve is drawn in 10px segments starting at the origin. 
+ * drawSine(時間, 波の幅のzoom, 波の開始位置の遅れ)
+ */
+function drawSine(canvas, t, zoom, delay) {
+    var xAxis = Math.floor(canvas.height/2);
+    var yAxis = 0;
+    var context = canvas.contextCache;
+    // Set the initial x and y, starting at 0,0 and translating to the origin on
+    // the canvas.
+    var x = t; //時間を横の位置とする
+    var y = Math.sin(x)/zoom;
+    context.moveTo(yAxis, unit*y+xAxis); //スタート位置にパスを置く
+
+    // Loop to draw segments (横幅の分、波を描画)
+    for (i = yAxis; i <= canvas.width + 10; i += 10) {
+        x = t+(-yAxis+i)/unit/zoom;
+        y = Math.sin(x - delay)/3;
+        context.lineTo(i, unit*y+xAxis);
+    }
+}
+init();
+
+$(function(){
+    $(".more").on("click", function() {
+        $(this).toggleClass("on-click");
+        $(".txt-hide").slideToggle(1000);
+    });
+}); 
+
+$(function () {
+$('.inner-box2').hide(); // 初期状態を非表示に
+$('.inner-box1').on('click', function () {
+    var findElm = $(this).next(".inner-box2");//直後のアコーディオンを行うエリアを取得し
+    $(findElm).slideToggle(500);//アコーディオンの上下動作
+
+// $('.down').addClass('active');
+// if($(this).hasClass('active')){
+//     $(this).removeClass('active');
+//     }
+});
+});                            
+
+
+//アコーディオンをクリックした時の動作
+// $('.inner-box1').on('click', function() {//タイトル要素をクリックしたら
+//     var findElm = $(this).next(".inner-box2");//直後のアコーディオンを行うエリアを取得し
+//     $(findElm).slideToggle();//アコーディオンの上下動作
+    
+//     if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+//         $(this).removeClass('close');//クラス名を除去し
+//     }else{//それ以外は
+//         $(this).toggleClass('close');//クラス名closeを付与
+//     }
+// });
+
+
+// //ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+// $(window).on('load', function(){
+// $('.inner-box2').addClass(".open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+// $(".open").each(function(index, element){ //openクラスを取得
+//     var Title =$(element).children('.inner-box2'); //openクラスの子要素のtitleクラスを取得
+//     $(Title).addClass('close');       //タイトルにクラス名closeを付与し
+//     var Box =$(element).children('.inner-box2'); //openクラスの子要素boxクラスを取得
+//     $(Box).slideDown(1000);          //アコーディオンを開く
+// });
+// });
